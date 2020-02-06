3f69f2ff8c2b33bbef9a2ed409e93fa853df9f9c
gerry0509 <39445714+gerry0509@users.noreply.github.com>
2020-02-05 21:10:46 -0600

    Update program.py

--- program.py
+++ program.py
@@ -1,7 +1,13 @@
 import fileinput
 lines=[]
 suma=0
+flotante=0
 for line in fileinput.input():
+	if '.' in line:
+		flotante=1;	
 	lines.append(line)
 	suma=suma+float(line)
-print(suma)
+if flotante=1:
+	print(suma)
+else:
+	print(int(suma))
